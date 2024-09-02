c6c61452a862db301f981e2500877a0bcde30e5d
JC4F <lequan12122002@gmail.com>
2024-09-02 15:57:51 +0700

    add scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
-  css: ["~/assets/css/main.css"],
+  css: ["~/assets/scss/main.scss"],
   shadcn: {
     /**
      * Prefix for all the imported component
