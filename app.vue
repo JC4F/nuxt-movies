f58598c9084e0dce3f6417552711f5656331226f
JC4F <lequan12122002@gmail.com>
2024-09-02 22:00:13 +0700

    copy files through project

--- app.vue
+++ app.vue
@@ -1,7 +1,66 @@
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  charset: "utf-8",
+  title: "Nuxt Movies",
+  titleTemplate: (title) =>
+    title !== "Nuxt Movies" ? `${title} · Nuxt Movies` : title,
+  meta: [
+    {
+      name: "description",
+      content:
+        "A TMDB client built with Nuxt Image to show the potential of it ✨",
+    },
+    {
+      property: "og:image",
+      content: "https://nuxt-movies-mu.vercel.app/social-card.png",
+    },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:site", content: "@nuxt_js" },
+    { name: "twitter:creator", content: "@nuxt_js" },
+  ],
+  link: [
+    {
+      rel: "icon",
+      type: "image/webp",
+      href: "/movies.webp",
+    },
+  ],
+});
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-    <Button>Click me</Button>
+  <NuxtLoadingIndicator />
+  <div
+    h-full
+    w-full
+    font-sans
+    grid="~ lt-lg:rows-[1fr_max-content] lg:cols-[max-content_1fr]"
+    of-hidden
+    view-transition-app
+    transition
+    duration-0
+  >
+    <div id="app-scroller" of-x-hidden of-y-auto relative>
+      <NuxtPage />
+    </div>
+    <NavBar lg:order-first />
+    <IframeModal />
+    <PhotoModal />
   </div>
 </template>
+
+<style>
+html,
+body,
+#__nuxt {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  background: #111;
+  color: white;
+  color-scheme: dark;
+}
+</style>
