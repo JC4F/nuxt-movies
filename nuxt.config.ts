761d085565a92f660f068011c4f18921f9f9a8c0
Haaland Le <haaland.le@techvify.com.vn>
2024-09-05 09:06:08 +0700

    remove html validator

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,6 @@ export default defineNuxtConfig({
     "@vueuse/nuxt",
     "@nuxt/image",
     "@nuxtjs/i18n",
-    "@nuxtjs/html-validator",
     "@nuxt/eslint",
     "@nuxtjs/color-mode",
   ],
@@ -128,33 +127,6 @@ export default defineNuxtConfig({
     lazy: true,
     langDir: "internationalization",
     defaultLocale: "en",
-  },
-  htmlValidator: {
-    usePrettier: false,
-    logLevel: "verbose",
-    failOnError: false,
-    /** A list of routes to ignore (that is, not check validity for). */
-    ignore: [/\.(xml|rss|json)$/],
-    options: {
-      extends: [
-        "html-validate:document",
-        "html-validate:recommended",
-        "html-validate:standard",
-      ],
-      rules: {
-        "svg-focusable": "off",
-        "no-unknown-elements": "error",
-        // Conflicts or not needed as we use prettier formatting
-        "void-style": "off",
-        "no-trailing-whitespace": "off",
-        // Conflict with Nuxt defaults
-        "require-sri": "off",
-        "attribute-boolean-style": "off",
-        "doctype-style": "off",
-        // Unreasonable rule
-        "no-inline-style": "off",
-      },
-    },
   },
   colorMode: {
     preference: "light", // default value of $colorMode.preference
